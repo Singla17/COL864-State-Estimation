b0f1e3b9ab7d6f5a95c347a7a3ef2169400c57e1
lakshya620 <lakshyatangri11@gmail.com>
2022-02-25 23:00:49 +0530

    Observation Model

--- Q3/Robot.py
+++ Q3/Robot.py
@@ -16,6 +16,7 @@ class Robot:
         self.state=init_state
         self.r_max=r_max
         self.grid=(grid)
+        self.path=[init_state]
         
     def updateState(self):
         """
@@ -33,18 +34,44 @@ class Robot:
         if  possible_pos is None:
             print(f"Locked in Grid at pos ({x},{y}).Cannot Move") 
         self.state=possible_pos[(np.random.randint(0,len(possible_pos)))]
-
+        self.path.append(self.state)
 
         
     def getObservation(self):
-        pass
+        """
+        Returns a 4x1  discrete vector denoting observation from each sensor
+        Each sensor gives a discrete binary observation {dn,ds,de,dw}
+        whether  a wall is close (1) or far(0) 
+        """
+        z_t=np.zeros((4,1))
 
+        pos=self.getTuple()
+        dist_N=self.grid.obsDistance_N(pos)
+        dist_S=self.grid.obsDistance_S(pos)
+        dist_W=self.grid.obsDistance_W(pos)
+        dist_E=self.grid.obsDistance_E(pos)
+        Rmax=self.r_max
+        prob_N=1-((dist_N-1)/(Rmax-1)) if dist_N<Rmax else 0
+        z_t[0][0]=np.random.binomial(1,prob_N)
+        prob_S=1-((dist_S-1)/(Rmax-1)) if dist_S<Rmax else 0
+        z_t[1][0]=np.random.binomial(1,prob_S)
+        prob_E=1-((dist_E-1)/(Rmax-1)) if dist_E<Rmax else 0
+        z_t[2][0]=np.random.binomial(1,prob_E)
+        prob_W=1-((dist_W-1)/(Rmax-1)) if dist_W<Rmax else 0
+        z_t[3][0]=np.random.binomial(1,prob_W)
+        # print(prob_N,prob_S,prob_E,prob_W)
+        return z_t
+
+    def getTuple(self):
+        return (self.state[0][0],self.state[1][0])
 
 if __name__=="__main__":
     g=Grid(9,5,[(3,1),(3,2),(4,1),(4,2)])
     # visualise(g)
-    init_state=np.array([2,1])[:,np.newaxis]
+    init_state=np.array([1,3])[:,np.newaxis]
     robo=Robot(init_state,5,g)
     robo.updateState()
+    print(robo.path)
     print(robo.state)
-    visualise(g)
+    print(robo.getObservation())
+    # visualise(g)
